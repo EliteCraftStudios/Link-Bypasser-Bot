648d5eeb6bed66f1ccd2ddca354a8c102c43e46c
♚ God Tanjiro ♚ <136969343+EliteCraftStudios@users.noreply.github.com>
2023-09-09 20:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ import re
 bot_token = os.environ.get("TOKEN", "5964615337:AAFEFoOA2OZqZsmC8jc6i3sts75137YIdng")
 api_hash = os.environ.get("HASH", "ab6f9f397bdd0c5c26f7fa419e1a205f") 
 api_id = os.environ.get("ID", "23148191")
-OWNER_ID = os.environ.get("OWNER_ID", "")
+OWNER_ID = os.environ.get("OWNER_ID", "6100029268")
 ADMIN_LIST = [int(ch) for ch in (os.environ.get("ADMIN_LIST", f"{OWNER_ID}")).split()]
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "god_luffy_ati")
 PERMANENT_GROUP = os.environ.get("PERMANENT_GROUP", "-100")
 GROUP_ID = [int(ch) for ch in (os.environ.get("GROUP_ID", f"{PERMANENT_GROUP}")).split()]
-UPDATES_CHANNEL = str(os.environ.get("UPDATES_CHANNEL", ""))
+UPDATES_CHANNEL = str(os.environ.get("UPDATES_CHANNEL", "-1001949726277"))
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 # handle ineex
